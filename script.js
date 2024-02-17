d8f1abd25db8ca4be2d2b6398551a251156879eb
Henok <henokasegdew110@gmail.com>
2024-02-17 11:05:58 +0300

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const openBtn = document.getElementById('hum-btn');
 const closeBtn = document.getElementById('back-btn');
 const mobileMenu = document.getElementById('mobile-menu');
-// const menuContent = document.querySelectorAll('.menu-content');
 const wrapper = document.getElementById('wrapper');
 const layer = document.getElementById('layer');
 const dropBtns = document.querySelectorAll('.drop-btn');
@@ -25,6 +24,7 @@ function closeMenu() {
 
 openBtn.addEventListener('click', openMenu);
 closeBtn.addEventListener('click', closeMenu);
+layer.addEventListener('click', closeMenu);
 dropBtns.forEach(dropBtn => {
     dropBtn.addEventListener('click', () => {
         dropBtn.classList.toggle('text-lightViolet');
